273ecf1f29c582842e7250022daf5e22ec341d13
duongf7 <duongf7@gmail.com>
2021-07-01 06:49:52 +0700

    Updates

---
+++ shader/vertex.glsl
@@ -0,0 +1,10 @@
+varying vec2 vertexUV;
+
+varying vec3 vertexNormal;
+
+
+void main() {
+    vertexNormal = normalize(normalMatrix * normal);
+    vertexUV = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
