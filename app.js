fbfd834430111d4bdcfb30c6ab9a0550c3a5fe8d
duongf7 <86104649+duongf7@users.noreply.github.com>
2021-07-01 12:23:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const atmosphereFragmentShader =`
 varying vec3 vertexNormal;
 void main() {
     float intensity = pow(0.95 - dot(vertexNormal, vec3(0, 0, 1)), 2.0);
-    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
+    gl_FragColor = vec4(0.3, 0.6, 1.0, 0.6) * intensity;
     }
 `;
 
